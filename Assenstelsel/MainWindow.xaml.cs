c9fb6a9b6388c0e215e687adf39ff03c6a059c99
Mike Overbeek <mikeoverbeek@outlook.com>
2020-06-18 00:18:36 +0200

    ed

--- Assenstelsel/MainWindow.xaml.cs
+++ Assenstelsel/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace Assenstelsel
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point clickPoint;
         public MainWindow()
         {
             InitializeComponent();
+            clickPoint = new Point(0, 0);
         }
 
 
@@ -31,40 +33,57 @@ namespace Assenstelsel
         {
             Point pointToWindow = Mouse.GetPosition(this);
             Point pointToScreen = PointToScreen(pointToWindow);
-            LiveCords.Text = $"Live information\nWiskundigecoördinaten: ({pointToWindow})\nMiddelpunt:\nBeelschermcoördinaten: ({pointToScreen})";
+            
+
+            double newX = pointToWindow.X - clickPoint.X ;
+            double newY = clickPoint.Y - pointToWindow.Y;
+
+            
+            LiveCords.Text = $"Live information\nWiskundigecoördinaten: ({pointToWindow})\nMiddelpunt: ({newX},{newY})\nBeelschermcoördinaten: ({pointToScreen})";
+            
         }
 
         private void LeftClick(object sender, MouseButtonEventArgs e)
         {
-            Point pointToWindow = Mouse.GetPosition(this);
-            Point pointToScreen = PointToScreen(pointToWindow);
-            LockedCords.Text = $"Locked information\nWiskundigecoördinaten: ({pointToWindow})\nMiddelpunt:\nBeelschermcoördinaten: ({pointToScreen})";
+            // tracking cords
+            Point pointToWindow = Mouse.GetPosition(MainCanvas);
 
             // https://docs.microsoft.com/en-us/dotnet/framework/wpf/graphics-multimedia/how-to-create-a-line-using-a-linegeometry
-            LineGeometry VerticalLine = new LineGeometry();
-            VerticalLine.StartPoint = new Point(pointToWindow.X,0);
-            VerticalLine.EndPoint = new Point(pointToWindow.X, MainCanvas.ActualHeight);
-           
-            Path VerticalPath = new Path();
-            VerticalPath.Stroke = Brushes.Red;
-            VerticalPath.StrokeThickness = 1;
-            VerticalPath.Data = VerticalLine;
 
-            LineGeometry HorizontalLine = new LineGeometry();
-            HorizontalLine.StartPoint = new Point(0, pointToWindow.Y);
-            HorizontalLine.EndPoint = new Point(MainCanvas.ActualWidth, pointToWindow.Y);
+            // Vertical line
+            LineGeometry VerticalLine = new LineGeometry
+            {
+                StartPoint = new Point(pointToWindow.X, 0),
+                EndPoint = new Point(pointToWindow.X, MainCanvas.ActualHeight)
+            };
 
-            Path HorizontalPath = new Path();
-            HorizontalPath.Stroke = Brushes.Red;
-            HorizontalPath.StrokeThickness = 1;
-            HorizontalPath.Data = HorizontalLine;
+            Path VerticalPath = new Path
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 3,
+                Data = VerticalLine
+            };
 
+            // Horizontal line
+            LineGeometry HorizontalLine = new LineGeometry
+            {
+                StartPoint = new Point(0, pointToWindow.Y),
+                EndPoint = new Point(MainCanvas.ActualWidth, pointToWindow.Y)
+            };
+
+            Path HorizontalPath = new Path
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 3,
+                Data = HorizontalLine
+            };
 
             // clear lines if exists
             MainCanvas.Children.Clear();
             // add lines to canvas
             MainCanvas.Children.Add(VerticalPath);
             MainCanvas.Children.Add(HorizontalPath);
+            clickPoint = pointToWindow;
         }
     }
 }
---
+++ Assenstelsel/Punt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assenstelsel
+{
+    public class Punt
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int X_Beeldscherm { get; set; }
+        public int Y_Beelscherm { get; set; }
+        public int rasterX { get; set; }
+        public int rasterY { get; set; }
+        public int Kleur { get; set; }
+        public int breedteRand { get; set; }
+        public int randKleur { get; set; }
+
+
+        public string rasterCords => rasterX.ToString() + rasterY.ToString();
+
+    }
+}
